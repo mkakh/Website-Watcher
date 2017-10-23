5a37ce61ad65cab9bb7f09b7aab474fd0034e3d1
akh <akh@mkakh.com>
2017-10-23 09:00:09 +0900

    Fix ww.sh

---
+++ README.sh
@@ -0,0 +1,10 @@
+# Website Watcher
+
+一定時間ごとにサイトにアクセスし，更新があったときにMP3を再生するスクリプトです．
+curl, diff, mpg123を使用しています．
+ww.shの変数を好みのものに変えてから使ってください．
+
+## Usage
+```
+$ ./ww.sh https://www.mkakh.com
+```
--- ww.sh
+++ ww.sh
@@ -4,18 +4,21 @@ set -eu
 
 ## EDIT HERE ####################
 INTERVAL=30s
-TARGET="http://www.mkakh.com/"
 MP3="${HOME}/Music/hogefuga.mp3"
+DIFF_OPTIONS="-b"
 #################################
 
 if [ $# -ne 1 ]; then
     echo "Usage: $0 [target]"
+    exit 1
 fi
 
+TARGET="$1"
 DIFF="$(curl $TARGET 2>/dev/null)"
 
+echo "-- START WATCHING --"
 while true; do
-    diff <(curl "$TARGET" 2>/dev/null) <(echo "$DIFF")
+    diff ${DIFF_OPTIONS} <(curl "$TARGET" 2>/dev/null) <(echo "$DIFF") \
             || mpg123 "$MP3"
     sleep ${INTERVAL}
 done
