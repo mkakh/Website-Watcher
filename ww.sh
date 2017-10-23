9649e4bb74a988e78e320eeaa142398e98c21be4
akh <akh@mkakh.com>
2017-10-23 09:15:36 +0900

    Fix ww.sh

--- ww.sh
+++ ww.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ## EDIT HERE ####################
-INTERVAL=10s
+INTERVAL=3s
 MP3="${HOME}/Music/hogefuga.mp3"
 DIFF_OPTIONS=""
 #################################
@@ -14,11 +14,16 @@ if [ $# -ne 1 ]; then
 fi
 
 TARGET="$1"
-DIFF="$(curl $TARGET 2>/dev/null)"
+
+function get_html() {
+    curl "$TARGET" 2>/dev/null
+}
+
+DIFF="$(get_html)"
 
 echo "-- START WATCHING --"
 while true; do
-    diff ${DIFF_OPTIONS} <(curl "$TARGET" 2>/dev/null) <(print "$DIFF") \
+    diff ${DIFF_OPTIONS} <(echo "$(get_html)") <(echo "$DIFF") \
             || mpg123 "$MP3"
     sleep ${INTERVAL}
 done
